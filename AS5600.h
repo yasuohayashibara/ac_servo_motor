ae7b25aebb25f6b3b2bee9e3aeacc0e81518d58a
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2016-02-29 22:26:26 +0900

    サンプルを追加

--- AS5600.h
+++ AS5600.h
@@ -5,6 +5,31 @@
 
 #include "mbed.h"
 
+#include "mbed.h"
+#include "AS5600.h"
+
+/** Class to measure angle using an absolute encoder AS5600
+ *
+ * Exmaple
+ * @code
+ *
+ * // Display Angle (Button 1: Reset Angle)
+ * BusOut led(LED1, LED2, LED3, LED4);
+ * BusIn sw(SW1, SW2);
+ * AS5600 as5600(I2C_SDA, I2C_SCL);
+ * 
+ * int main() {
+ * 	led = 0;
+ * 	sw.mode(PullUp);
+ * 	while(1) {
+ *		float angle = as5600;
+ *		printf("%f\r\n", angle);
+ *		if (sw[0] == 0) as5600 = 0;
+ *		wait(0.5);
+ *	}
+ *}
+ */
+
 class AS5600
 {
 public:
